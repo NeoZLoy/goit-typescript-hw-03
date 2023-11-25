9fba927ceb1a693a96a6b246014059f494e31eb2
Michael <fidd.michael@gmail.com>
2023-11-25 20:33:39 +0100

    done

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,52 @@
-const key = new Key();
+class Key{
+    constructor(protected signature: number){
+        signature = Math.random();
+    }
+
+    getSignature():number {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key){
+        this.key = key
+    }
+
+    getKey():Key{
+        return this.key
+    }
+}
+
+
+abstract class House{
+    constructor(protected key: Key){
+        this.key = key;
+    }
+    protected door : boolean = false;
+    protected tenants :Person[] = [];
+    comeIn(person:Person){
+        if(this.door){
+            this.tenants.push(person)
+        }
+    }
+    abstract openDoor(key:Key):void
+}
+
+class MyHouse extends House{
+   constructor(key: Key){
+    super(key)
+   }
+
+   openDoor(key: Key): void {
+       if(key.getSignature() === this.key.getSignature()){
+        this.door = true;
+       }
+   }
+}
+
+
+const key = new Key(3);
 
 const house = new MyHouse(key);
 const person = new Person(key);
